6fef067741792dc2126409008f1cc8c577dd1520
Piotr <piotr.slobinski@gmail.com>
2021-06-09 16:43:46 +0200

    added Card & Container components

---
+++ first-tutorial-from-yt/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='card'>
+      <p classname='text'>this is text inside of a card</p>
+    </div>
+  )
+}
+
+export default Card
---
+++ first-tutorial-from-yt/src/components/Container.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+    <div className='container spacer'>
+      <p className='text'>
+        lorem ipsum dolor sit ammet.....
+      </p>
+    </div>
+  )
+}
+
+export default Container
