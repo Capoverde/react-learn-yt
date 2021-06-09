48ca06db242aa0b37e70c255a5e40fef4beee743
Piotr <piotr.slobinski@gmail.com>
2021-06-09 16:00:36 +0200

    first commit in intro-1

--- first-tutorial-from-yt/src/App.js
+++ first-tutorial-from-yt/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './styles/App.scss';
+import './styles/App.scss'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+    <div className='App'>
+      <header className='App-header'>
+        <div className='container'>
+          <p className='spacer'>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          </p>
+          <a
+            className='App-link spacer'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
           Learn React
-        </a>
+          </a>
+        </div>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
